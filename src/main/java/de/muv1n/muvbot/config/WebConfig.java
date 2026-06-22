c103c3c630016045793d46dd82a2d7bb9ca36cff
Marvin Kleiner <marvin.kleiner@fricke.de>
2026-06-22 12:27:05 +0200

    feat: add LoggingInterceptor to WebConfig for request logging

--- src/main/java/de/muv1n/muvbot/config/WebConfig.java
+++ src/main/java/de/muv1n/muvbot/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -22,6 +23,11 @@ public class WebConfig {
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
+            }
+
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new LoggingInterceptor());
             }
         };
     }
