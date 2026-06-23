851cb2df98d3c99bd5fa6f054e76b2241ec54cfb
Marvin Kleiner <marvin.kleiner@fricke.de>
2026-06-23 14:33:26 +0200

    feat: add logger and database config logging to MuvBotApplication
    - Integrated SLF4J logger for improved application logging.
    - Added database configuration log output to assist with debugging and setup visibility.

--- src/main/java/de/muv1n/muvbot/MuvBotApplication.java
+++ src/main/java/de/muv1n/muvbot/MuvBotApplication.java
@@ -1,11 +1,15 @@
 package de.muv1n.muvbot;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MuvBotApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(MuvBotApplication.class);
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure()
@@ -21,6 +25,10 @@ public class MuvBotApplication {
         setIfPresent(dotenv, "DISCORD_CLIENT_ID");
         setIfPresent(dotenv, "DISCORD_CLIENT_SECRET");
         setIfPresent(dotenv, "DISCORD_REDIRECT_URI");
+        logger.info("\n\nDatabase configuration: jdbcUrl={}, user={}, password={}\n\n",
+                isConfigured("DB_USER"),
+                isConfigured("DB_URL"),
+                isConfigured("DB_PASS"));
 
         SpringApplication.run(MuvBotApplication.class, args);
     }
@@ -30,5 +38,9 @@ public class MuvBotApplication {
         if (value != null && !value.isBlank()) {
             System.setProperty(key, value);
         }
+    }
+    private static String isConfigured(String key) {
+        String value = System.getProperty(key);
+        return value;
     }
 }
